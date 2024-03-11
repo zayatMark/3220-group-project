2293654fb1c21b4c68cc5dd0c23b767e8d1eb29a
Eli-Par <59713280+Eli-Par@users.noreply.github.com>
2024-03-10 21:11:58 -0400

    Updated vertical data list item description formatting Made the description always start 35% of the way across the item, no matter the length of the title.

--- src/HomePage/VerticalDataList/VerticalDataListItem.js
+++ src/HomePage/VerticalDataList/VerticalDataListItem.js
@@ -13,19 +13,23 @@ class VerticalDataListItem extends Component {
             <li className="list-item">
                 {/* Button to click that will allow for viewing that piece of data */}
                 <button className="list-button">
-                    {/* Data icon centred vertically and left aligned */}
-                    <span style={{verticalAlign: "middle", margin: "auto 0", padding: "0.5rem 0 0.5rem 1rem"}}>
-                        <img className='list-icon' src={(this.props.data.getIcon() === null) ? defaultIcon : this.props.data.getIcon()} alt="" />
+                    <span style={{width:"35%", display: "flex"}}>
+                        {/* Data icon centred vertically and left aligned */}
+                        <span style={{verticalAlign: "middle", margin: "auto 0", padding: "0.5rem 0 0.5rem 1rem"}}>
+                            <img className='list-icon' src={(this.props.data.getIcon() === null) ? defaultIcon : this.props.data.getIcon()} alt="" />
+                        </span>
+
+                        {/* Data title, vertically centred */}
+                        <span style={{verticalAlign: "middle", padding: "1rem", margin: "auto 0", color: "rgb(50, 50, 50)"}}>
+                            <h3> {this.props.data.getTitle()} </h3>
+                        </span>
                     </span>
 
-                    {/* Data title, vertically centred */}
-                    <span style={{verticalAlign: "middle", padding: "1rem", margin: "auto 0", color: "rgb(50, 50, 50)"}}>
-                        <h3> {this.props.data.getTitle()} </h3>
-                    </span>
-
-                    {/* Data short description, vertically centred and slightly grayed out */}
-                    <span style={{verticalAlign: "middle", padding: "1rem", margin: "auto 0", color: "rgb(30, 30, 30)"}}>
-                        {this.props.data.getShortDescription()}
+                    <span style={{width:"65%", display: "flex"}}>
+                        {/* Data short description, vertically centred and slightly grayed out */}
+                        <span style={{verticalAlign: "middle", padding: "1rem", margin: "auto 0", color: "rgb(30, 30, 30)"}}>
+                            {this.props.data.getShortDescription()}
+                        </span>
                     </span>
                 </button>
             </li>

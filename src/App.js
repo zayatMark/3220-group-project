d66e9b1a40fa610551b1f0e2ea7db18b2ede7e3f
hyeleenotharry <dlgpgl1029@gmail.com>
2024-03-12 05:51:32 +0900

    data overview page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Overview from './components/detail-view'
+
+function App() {
+  return (
+    <div className="App">
+      <Overview />
+    </div>
+  );
+}
+
+export default App;

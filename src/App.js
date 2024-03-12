ecf28f4c95f505d86e1e77a0c125ef40f69d4d9b
Eli-Par <59713280+Eli-Par@users.noreply.github.com>
2024-03-11 20:22:01 -0400

    Added react router Added react router to the project, setting it up in the App class. This will allow for the various pages to be navigated between. Also added a temporary top bar to allow for navigation testing.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,52 @@ import './App.css';
 
 import React, { Component} from 'react'
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import MultiDataView from './multi-data-view/multidata';
 import HomePage from './HomePage/HomePage';
+import Overview from './components/detail-view'
 
 class App extends Component {
 
   render() {
     return (
-      <HomePage />
+      <BrowserRouter>
+        <div className="App">
+
+          {/* Temporary navitation list */}
+          <ul style={{listStyleType:"none", backgroundColor:"rgb(200, 200, 200)", marginTop:"0", padding:"1rem"}}>
+            <li>
+              <Link to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/filter-data">
+                  Filter Data
+              </Link>
+            </li>
+            <li>
+              <Link to="/data">
+                  View Data
+              </Link>
+            </li>
+          </ul>
+
+          {/* Define all routes to the various pages */}
+          <Routes>
+            <Route path="/" element={<HomePage />} ></Route>
+            <Route path="/filter-data" element={<MultiDataView />}></Route>
+            <Route path="/data" element={<Overview />}></Route>
+          </Routes>
+
+        </div>
+      </BrowserRouter>
     );
   }
 

dbc8d4ecf4bbfcbffa2f2b926f0cd97e7afb621a
Eli-Par <59713280+Eli-Par@users.noreply.github.com>
2024-03-16 09:16:14 -0400

    Added missing icons to the recent data list

--- src/HomePage/VerticalDataList/VerticalDataList.js
+++ src/HomePage/VerticalDataList/VerticalDataList.js
@@ -15,6 +15,10 @@ import DataItem from './DataItem';
 import treeIcon from './imgs/Tree-Icon.jpg'
 import rainIcon from './imgs/Rain-Icon.jpg'
 import garbageIcon from './imgs/Garbage-Icon.jpg'
+import broomIcon from './imgs/Broom-Icon.jpg'
+import dogIcon from './imgs/Dog-Icon.jpg'
+import leafIcon from './imgs/Leaf-Icon.jpg'
+import playgroundIcon from './imgs/Playground-Icon.jpg'
 
 import Cookies from 'universal-cookie';
 
@@ -25,13 +29,13 @@ class VerticalDataList extends Component {
 
         //List of page data for the brief descriptions of the pages
         this.dataPages =  [
-            new DataItem(1, null, "Yard Waste Not Collected Service Requests", "Information on the reports of uncollected yard waste within the city."),
+            new DataItem(1, leafIcon, "Yard Waste Not Collected Service Requests", "Information on the reports of uncollected yard waste within the city."),
             new DataItem(2, treeIcon, "Tree Requests", "Data on the maintenance of municipal trees that is initiated by customer service requests."),
-            new DataItem(3, null, "Street Sweeping Service Requests", "Dataset of customer initiated service requests for residential street cleaning."),
+            new DataItem(3, broomIcon, "Street Sweeping Service Requests", "Dataset of customer initiated service requests for residential street cleaning."),
             new DataItem(4, rainIcon, "Precipitation", "Information about precipitation in the city at various locations in mm/hr."),
-            new DataItem(5, null, "Dead Animal Removal Service Requests", "Data on the removal of dead dogs or cats on public property that is initiated by customer service requests."),
+            new DataItem(5, dogIcon, "Dead Animal Removal Service Requests", "Data on the removal of dead dogs or cats on public property that is initiated by customer service requests."),
             new DataItem(6, garbageIcon, "Garbage & Recycling Collection", "Information on the collection boundaries for garbage and recycling within the city."),
-            new DataItem(7, null, "Parks Playgrounds Service Requests", "Data about customer initiated service requests of service required for playgrounds.")
+            new DataItem(7, playgroundIcon, "Parks Playgrounds Service Requests", "Data about customer initiated service requests of service required for playgrounds.")
         ]
 
         this.componentDidMount();
